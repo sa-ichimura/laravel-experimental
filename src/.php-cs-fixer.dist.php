55c60fcbe755d44a41808b7bd2c6a6779732d31b
sa-ichimura <35563295+sa-ichimura@users.noreply.github.com>
2023-03-04 23:19:26 +0900

    Feature/add php cs fixer (#3)
    * add php-cs-fixer
    
    * ルールの記述順番変更

---
+++ src/.php-cs-fixer.dist.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This document has been generated with
+ * https://mlocati.github.io/php-cs-fixer-configurator/#version:3.1.0|configurator
+ * you can change this configuration by importing this file.
+ */
+$config = new PhpCsFixer\Config();
+return $config
+    ->setRules([
+        // https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/blob/master/doc/ruleSets/PSR12.rst
+        '@PSR12' => true,
+        // https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/blob/master/doc/ruleSets/PHP82Migration.rst
+        '@PHP82Migration' => true,
+        // https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/blob/master/doc/ruleSets/PhpCsFixer.rst
+        '@PhpCsFixer' => true,
+        'self_static_accessor' => true,
+        'new_with_braces' => false,
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            // ->exclude('folder-to-exclude') // if you want to exclude some folders, you can do it like this!
+            ->in(__DIR__)
+    );
